c9f390e2c474591c87ca82715fdc92ec55ec6e0b
Dmitry Panchuk <10994198@uvu.edu>
2024-09-04 22:15:47 -0600

    Completed the Unity Events Challenge

--- IntroAssignments/Assets/Scripts/TiggerEventBehaviour.cs
+++ IntroAssignments/Assets/Scripts/TiggerEventBehaviour.cs
@@ -3,10 +3,25 @@ using UnityEngine;
 
 public class TiggerEventBehaviour : MonoBehaviour
 {
-    public UnityEvent triggerEnterEvent;
+    public UnityEvent triggerEnterEvent, onEnableEvent, onDisableEvent, onStartEvent;
 
     private void OnTriggerEnter(Collider other)
     {
         triggerEnterEvent.Invoke();
+    }
+
+    private void OnEnable()
+    {
+        onEnableEvent.Invoke();
+    }
+
+    private void OnDisable()
+    {
+        onDisableEvent.Invoke();
+    }
+
+    private void Start()
+    {
+        onStartEvent.Invoke();
     }
 }
